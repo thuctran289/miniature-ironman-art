7c2aa93d031055db627c4d2df91d7526cef92c7d
shanek21 <shanegkelly21@gmail.com>
2015-03-09 20:11:09 -0400

    Worked on color tracking

--- color_detect.py
+++ color_detect.py
@@ -1,19 +1,32 @@
 import cv2
 import numpy as np
+import time
 
 #Start cam stream
 cap = cv2.VideoCapture(0)
 
 while(1):
-
     #Set 'frame' as the current cam capture
     _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    colorSegment = frame[10:110, 430:530]
+    cv2.rectangle(frame, (10, 430), (110, 530), (0, 0, 255), 1)
+
+    b,g,r = cv2.split(colorSegment)
+
+    print np.average(b)
+    print np.average(g)
+    print np.average(r)
+    print '\n'
+
+    time.sleep(.2)
 
     #Blur the frame
     blurframe = cv2.blur(frame, (50, 50))
 
     #Define color range in BGR
-    lower_BGR = np.array([180, 180, 180])
+    lower_BGR = np.array([238, 210, 210])
     upper_BGR = np.array([255, 255, 255])
 
     # Threshold the image to get only the selected colors
@@ -31,7 +44,7 @@ while(1):
         M = cv2.moments(cnt)
         x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(frame, (x+w/2-5,y+h/2-5), (x+w/2+5, y+h/2+5), (0, 0, 255), 3)
-        print x+w/2, y+h/2
+        # print x+w/2, y+h/2
 
     #Display the final image
     cv2.imshow('frame', frame)
@@ -42,6 +55,3 @@ while(1):
 #Release the capture and close the windows
 cap.release()
 cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    publisher()
