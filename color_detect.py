3cc584b39f3fd4fe74279c748b92ea9cbd85bb9d
Thuc <thuc@T.Tran>
2015-03-11 23:44:06 -0400

    commit for merge

--- color_detect.py
+++ color_detect.py
@@ -151,82 +151,85 @@ class RadiusBox(CommandBox):
         if self.r<=1:
             self.r = 1
 
-#Start cam stream
-cap = cv2.VideoCapture(0)
 
-frameWidth = cap.get(3)
-frameHeight = cap.get(4)
+if __name__ == '__main__':
+    #Start cam stream
+    cap = cv2.VideoCapture(0)
 
-pointer = Point(0, 0, 5)
-light = Light(0, 0, 0, 0)
-color_box = ColorBox(0, 430, 100, 100, [0, 0, 255],[0,0,0])
-radius_box = RadiusBox(int(frameWidth-150),int(frameHeight-50),100,50,[0,255,0],5)
-reset_box = ResetBox(int(frameWidth-50),int(frameHeight-50),50,50,[255,0,0])
-#Define color range in BGR
-lower_BGR = np.array([210, 150, 150])
-upper_BGR = np.array([255, 255, 255])
+    frameWidth = cap.get(3)
+    frameHeight = cap.get(4)
 
-points = []
+    pointer = Point(0, 0, 5)
+    light = Light(0, 0, 0, 0)
+    color_box = ColorBox(0, 430, 100, 100, [0, 0, 255],[0,0,0])
+    radius_box = RadiusBox(int(frameWidth-150),int(frameHeight-50),100,50,[0,255,0],5)
+    reset_box = ResetBox(int(frameWidth-50),int(frameHeight-50),50,50,[255,0,0])
+    #Define color range in BGR
+    lower_BGR = np.array([210, 150, 150])
+    upper_BGR = np.array([255, 255, 255])
 
-counter = 0
+    points = []
 
-while(1):
-    #Set 'frame' as the current cam capture
-    _, frame = cap.read()
-    #Flip the frame over the vertical axis (so that it looks like looking into a mirror)
-    frame = cv2.flip(frame, 1)
-    #Find the average color of the ColorBox by getting frame of the color, and inputting into function. 
-    color_segment = frame[color_box.y : color_box.y+color_box.h, color_box.x : color_box.x+color_box.w]
-    color_box.set_draw_color(color_segment)
+    counter = 0
 
-    #Guarantees a clean point each iteration on the loop
-    pointer.x = 0
-    pointer.y = 0
-    pointer.r = 0
+    while(1):
+        #Set 'frame' as the current cam capture
+        _, frame = cap.read()
+        #Flip the frame over the vertical axis (so that it looks like looking into a mirror)
+        frame = cv2.flip(frame, 1)
+        #Find the average color of the ColorBox by getting frame of the color, and inputting into function. 
+        color_segment = frame[color_box.y : color_box.y+color_box.h, color_box.x : color_box.x+color_box.w]
+        color_box.set_draw_color(color_segment)
 
-    #Blur the frame
-    blurframe = cv2.blur(frame, (50, 50))
-    # Threshold the image to get only the selected colors
-    mask = cv2.inRange(blurframe, lower_BGR, upper_BGR)
-    contour_mask = mask
-    cv2.imshow('mask', mask)
+        #Guarantees a clean point each iteration on the loop
+        pointer.x = 0
+        pointer.y = 0
+        pointer.r = 0
 
-    #Create a contour around the selected colors
-    contours,_ = cv2.findContours(contour_mask, 1, 2)
+        #Blur the frame
+        blurframe = cv2.blur(frame, (50, 50))
+        # Threshold the image to get only the selected colors
+        mask = cv2.inRange(blurframe, lower_BGR, upper_BGR)
+        contour_mask = mask
+        cv2.imshow('mask', mask)
 
-    #If there is a contour, add the position and current drawing color to a list
-    if len(contours) > 0:
-        cnt = contours[0]
-        M = cv2.moments(cnt)
-        #Define the color-tracked light source
-        light.x, light.y, light.w, light.h = cv2.boundingRect(cnt)
-        #Assign the position of the pointer
-        pointer.x = light.x
-        pointer.y = light.y
-        pointer.r = radius_box.r
-        #Add the pointer and color characteristics to a reference list
-        new_color = [color_box.draw_bgr[0],color_box.draw_bgr[1],color_box.draw_bgr[2]]
-        points.append([[pointer.x+light.w/2, pointer.y+light.h/2, pointer.r], new_color])
-    #If there is anything to be drawn, draw it!
+        #Create a contour around the selected colors
+        contours,_ = cv2.findContours(contour_mask, 1, 2)
 
-    counter += 1
-    if counter % 5 == 0:
-        radius_box.change_radius(pointer)
-    points = reset_box.check_reset(pointer, points)
+        #If there is a contour, add the position and current drawing color to a list
+        if len(contours) > 0:
+            cnt = contours[0]
+            M = cv2.moments(cnt)
+            #Define the color-tracked light source
+            light.x, light.y, light.w, light.h = cv2.boundingRect(cnt)
+            #Assign the position of the pointer
+            pointer.x = light.x
+            pointer.y = light.y
+            pointer.r = radius_box.r
+            #Add the pointer and color characteristics to a reference list
+            new_color = [color_box.draw_bgr[0],color_box.draw_bgr[1],color_box.draw_bgr[2]]
+            points.append([[pointer.x+light.w/2, pointer.y+light.h/2, pointer.r], new_color])
+        #If there is anything to be drawn, draw it!
 
-    if len(points) > 0:
-        for i in xrange(len(points)):
-            cv2.circle(frame, (points[i][0][0], points[i][0][1]), points[i][0][2], (points[i][1][0], points[i][1][1], points[i][1][2]), -1)
-    color_box.display_box(radius_box.r)
-    radius_box.display_box()
-    reset_box.display_box()
+        counter += 1
+        if counter % 5 == 0:
+            radius_box.change_radius(pointer)
+        points = reset_box.check_reset(pointer, points)
 
-    #Display the final image
-    cv2.imshow('frame', frame)
-    #If ESC pressed, quit the program
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        if len(points) > 0:
+            for i in xrange(len(points)):
+                cv2.circle(frame, (points[i][0][0], points[i][0][1]), points[i][0][2], (points[i][1][0], points[i][1][1], points[i][1][2]), -1)
+        
+        color_box.display_box(radius_box.r)
+        radius_box.display_box()
+        reset_box.display_box()
 
-#Release the capture and close the windows
-cap.release()
-cv2.destroyAllWindows()
+        #Display the final image
+        cv2.imshow('frame', frame)
+        #If ESC pressed, quit the program
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    #Release the capture and close the windows
+    cap.release()
+    cv2.destroyAllWindows()
